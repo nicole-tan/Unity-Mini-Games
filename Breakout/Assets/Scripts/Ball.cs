4a2018e46af0f49793efb4044d1b7415f6badf2b
Nicole Tan <nat34@cornell.edu>
2016-07-01 00:09:09 -0400

    Added beginning of physics needed for ball and paddle to wok

---
+++ Breakout/Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	//Ball is a child of Paddle because you want it to follow Paddle before Start
+
+	public float ballInitialVelocity = 600f;
+	//Rigidbody allows for control of an object's position through physics simulation
+		//needed so you can set isKinematic to false later
+	private Rigidbody rb;
+	private bool ballInPlay;
+
+	// Called before Start () function
+	void Awake () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Fire1 is left mouse key
+		if (Input.GetButtonDown ("Fire1") && ballInPlay == false) {
+			//unparent the ball from the ball so it can fly off 
+			transform.parent = null;
+			ballInPlay = true;
+			//isKinematic is turned on initially because you want to move the ball according to this script
+			//now it is turned off so forces [and Physics] can act on it
+			rb.isKinematic = false;
+			rb.AddForce (new Vector3 (ballInitialVelocity, ballInitialVelocity, 0));
+		}
+	}
+
+	//repeated physics should go into FixedUpdate (), but, as you are only dealing with adding force once, 
+	//you can leave it in Update () 
+}
--- Breakout/Assets/Scripts/Paddle.cs
+++ Breakout/Assets/Scripts/Paddle.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class Paddle : MonoBehaviour {
+	//isKinematic is turned on when you want to adjust an object using the script
+		//turned off when you want to use Physics
 	//Can be adjusted in the editor because it's public
 	public float paddleSpeed = 1f;
 
-	private Vector3 playerPos = new Vector3 (0, -9.5, 0);
+	private Vector3 playerPos = new Vector3 (0, -9.5f, 0);
 
 
 	// Update is called once per frame
@@ -16,7 +18,10 @@ public class Paddle : MonoBehaviour {
 		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
 
 		//Clamp limits a number between 2 values --> (xPos, min, max)
-		playerPos = new Vector3(Mathf.Clamp (xPos, -8f, 8f), -9.5, 0);
-
+		playerPos = new Vector3(Mathf.Clamp (xPos, -8f, 8f), -9.5f, 0);
+		transform.position = playerPos;
 	}
 }
+
+
+
