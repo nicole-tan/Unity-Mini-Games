59131e9a330ad97f537ee14641fd1e3ebd38c87d
Nicole Tan <nat34@cornell.edu>
2016-07-01 00:58:14 -0400

    Working on GameManager, particles and text complete

---
+++ Breakout/Assets/Scripts/GM.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour {
+
+	public int lives = 3;
+	public int bricks = 20;
+	public float resetDelay = 1f;
+	public Text livesText;
+	public GameObject gameOver;
+	public GameObject youWon;
+	public GameObject bricksPrefab;
+	public GameObject paddle;
+	public GameObject deathParticles;
+	//static variable because we're going to access it via the class, not via an instance of thec lass
+	//will be accessible from our other scripts [we can then get values like 'bricks'] from our game manager in a 
+	//	much easier fashion
+	public static GM instance = null;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
