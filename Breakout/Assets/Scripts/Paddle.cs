44886eb345677987606f3a10a717160bc13653ed
Nicole Tan <nat34@cornell.edu>
2016-06-30 22:56:56 -0400

    Created the beginnings of Breakout game (setup)

---
+++ Breakout/Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+	//Can be adjusted in the editor because it's public
+	public float paddleSpeed = 1f;
+
+	private Vector3 playerPos = new Vector3 (0, -9.5, 0);
+
+
+	// Update is called once per frame
+	void Update () {
+		//transform.position.x is the current position of x 
+		//x position transformed of the paddle + input of the left and right arrows on keyboard
+			//the position is determined by how much the arrows have been pressed and how fast the paddle moved
+		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+
+		//Clamp limits a number between 2 values --> (xPos, min, max)
+		playerPos = new Vector3(Mathf.Clamp (xPos, -8f, 8f), -9.5, 0);
+
+	}
+}
