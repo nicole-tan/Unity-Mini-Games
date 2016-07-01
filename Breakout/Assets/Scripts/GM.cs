5bce945c2a72927dd8af169b6328b4b34fe07ff5
Nicole Tan <nat34@cornell.edu>
2016-07-01 11:24:03 -0400

    Working on GameManager c

--- Breakout/Assets/Scripts/GM.cs
+++ Breakout/Assets/Scripts/GM.cs
@@ -13,19 +13,65 @@ public class GM : MonoBehaviour {
 	public GameObject bricksPrefab;
 	public GameObject paddle;
 	public GameObject deathParticles;
-	//static variable because we're going to access it via the class, not via an instance of thec lass
-	//will be accessible from our other scripts [we can then get values like 'bricks'] from our game manager in a 
-	//	much easier fashion
+	//static variable because we're going to access it via the class, not via an instance of the class
+	//will be accessible from our other scripts [we can then get values like 'bricks' from Brick] from our game manager 
+	//in a much easier fashion
+	//singleton pattern: only one instance of the Game Manager available
 	public static GM instance = null;
+
+	private GameObject clonePaddle; 
 
 
 	// Use this for initialization
 	void Start () {
+		//enforce singleton pattern
+			//do we have a Game Manager yet? 
+		if (instance == null) 
+			instance = this; 
 	
+		//ensures that you don't have two copies of your Game Manager
+		else if (instance != this)
+			Destroy (gameObject);
+
+		Setup (); 
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void Setup() {
+		//Quaternion.identity means no rotation
+		clonePaddle = Instantiate (paddle, transform.position, Quaternion.identity) as GameObject; 
+		Instantiate (bricksPrefab, transform.position, Quaternion.identity); 
+	}
+
+	void CheckGameOver() {
+		if (bricks < 1) {
+			youWon.SetActive (true);
+			Time.timeScale = .25f;
+			//allow you to call a function with a delay
+			Invoke ("Reset", resetDelay);
+		}
+
+		if (lives < 1) {
+			gameOver.SetActive (true);
+			Time.timeScale = .25f;
+			Invoke ("Reset", resetDelay); 
+		}
+			
+	}
+
+	void Reset () {
+		Time.timeScale = 1;
+		//used to load scenes
+		//all loadedlevel is is the last loaded level; only one scene so you're reloading the level
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	//will be accessed from other parts of the game
+	public void LoseLife() {
+		lives--;
+		livesText.text = "Lives: " + lives; 
+		//Clones the original object and returns the clone
+		Instantiate(deathParticles, clonePaddle.transform.position, Quaternion.identity);
+		Destroy (clonePaddle);
 	}
 }
